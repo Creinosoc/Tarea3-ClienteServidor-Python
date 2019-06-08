4ea61a12709c1dac4fd921cd61d7f316ab4b8909
Creinosoc <Creinosoc@ing.ucsc.cl>
2019-06-07 20:46:35 -0400

    Cliente Servidor funcional, se ordeno y borro archivos inneceserio, se agrego la funcion cachipun como juego

--- cliente.py
+++ cliente.py
@@ -33,47 +33,87 @@ def dado_calcular(dado,intentos):
 
 #-------------------------------------------------------
 
-print('Client Server...')
+if len(sys.argv) != 3:
+    print ("Agregar la IP del servidor y el puerto donde se ofrece el servicio.")
+    sys.exit(0)
+
+IP = sys.argv[1]
+PUERTO = int(sys.argv[2])
+
+try:
+    socket_cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket_cliente.connect((IP, PUERTO))
+except:
+    print ("No se puede conectar con el servidor.")
+    sys.exit(0)
+
+print('Iniciando Sesion CLIENTE...')
 time.sleep(1)
 #Get the hostname, IP Address from socket and set Port
-soc = socket.socket()
+# soc = socket.socket()
 shost = socket.gethostname()
 ip = socket.gethostbyname(shost)
 #get information to connect with the server
 print(shost, '({})'.format(ip))
-server_host = input('Enter server\'s IP address:')
-name = input('Enter Client\'s name: ')
-PUERTO = 9090
+server_host = 'IP'.format(IP)
+name = input('Ingrese su nombre: ')
 
-print("\nTrying to connect to ", server_host, "(", PUERTO, ")\n")
+# print("\nTrying to connect to ", server_host, "(", PUERTO, ")\n")
 # print('Trying to connect to the server: {}, ({})'.format(server_host, port))
 time.sleep(1)
-soc.connect((server_host, PUERTO))
-print("Connected...\n")
-soc.send(name.encode())
-server_name = soc.recv(2048)
+print("Conectando...\n")
+socket_cliente.send(name.encode())
+server_name = socket_cliente.recv(2048)
 server_name = server_name.decode()
 print('{} a entrado...'.format(server_name))
 print('Escriba <<salir()>> para salir de la sala')
 print('Espere la respuesta del servidor...')
 
-while True:
-    dado1 = []
-    message = soc.recv(2048)
-    message = message.decode()
-    print(message)  #imprime el mensaje del servidor
-    message = input(str("Yo >> "))
-    if message == 'salir()':
-        message = 'Leaving the Chat room'
-        soc.send(message.encode())
-        # print("\n")
-        break
-    if message == '1':
-        message = '1'
-        soc.send(message.encode())
-        print("\n")
-    if message == '':
-        j1 = dado_imprimir(dado1,intentos)
-        # print ("Has sacado saco un total de : ", j1)
-        message = "{}".format(j1)
-    soc.send(message.encode())
+try:
+
+    while True:
+        dado1 = []
+        message = socket_cliente.recv(2048)
+        message = message.decode()
+        print(message)  #imprime el mensaje del servidor
+        message = input(str("Yo >> "))
+        if message == 'salir()':
+            message = 'Adios!'
+            socket_cliente.send(message.encode())
+            break
+
+        if message == '1':
+            message = '1'
+            socket_cliente.send(message.encode())
+            print("\n")
+
+        if message == '':
+            j1 = dado_imprimir(dado1,intentos)
+            # print ("Has sacado saco un total de : ", j1)
+            message = "{}".format(j1)
+
+
+        if message == '2':
+            message = '2'
+            socket_cliente.send(message.encode())
+
+            message = socket_cliente.recv(2048)
+            message = message.decode()
+            print(message)  #imprime el mensaje del servidor
+            message = input(str("Yo >> "))
+            socket_cliente.send(message.encode())
+
+        if message == '3':
+            break
+
+        socket_cliente.send(message.encode())
+
+except socket.error:
+    print ("Se perdio la conexion con el servidor.")
+except KeyboardInterrupt:
+    print ("\nSe interrunpio el cliente con un Control_C.")
+
+finally:
+    print ("Terminando conexion con el servidor ...")
+    socket_cliente.close()
+    print ("Conexion con el servidor terminado.")
--- servidor.py
+++ servidor.py
@@ -1,6 +1,6 @@
 import time, socket, sys
 import random
-
+from time import sleep
 #-------------------------------------------------------
 caras = 6
 dado1 = []
@@ -30,74 +30,166 @@ def dado_clear(arg):
 # dado(dado1,intentos)
 #-------------------------------------------------------
 
+def cachipum():
+	mano_servidor = random.choice(["piedra", "papel", "tijera"])
+
+	return mano_servidor
+
+
+#-------------------------------------------------------
+
+if len(sys.argv) != 2:
+    print ("Agregar el puerto donde se va a ofrecer el servicio.")
+    sys.exit(0)
+
+IP = "192.168.122.89"
+IPLocal = "localhost"
+PUERTO = int(sys.argv[1])
+
 print('Configurando Servidor...')
-time.sleep(3)
+time.sleep(2)
 #Get the hostname, IP Address from socket and set Port
 
-soc = socket.socket()
+socket_servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# soc = socket.socket()
 host_name = "Sr servidor"
 IP = "localhost"
 # IP = socket.gethostbyname(host_name)
-PUERTO = 9090
-soc.bind((IP, PUERTO))
+socket_servidor.bind((IPLocal, PUERTO))
+# soc.bind((IP, PUERTO))
 print(host_name, '(IP = {})'.format(IP))
 # name = input('Enter name: ')
 name = "Sr Servidor"
-soc.listen(1) #Try to locate using socket
+socket_servidor.listen(1) #Try to locate using socket
 print('Esperando conexiones...')
-connection, addr = soc.accept()
-print("Received connection from ", addr[0], "(", addr[1], ")\n")
-print('Conexion establecida. conectado desde: {}, ({})'.format(addr[0], addr[0]))
-#get a connection from client side
-client_name = connection.recv(2048)
-client_name = client_name.decode()
-print(client_name + ' El cliente se a conectado.')
-print('Escriba <<salir()>> para Cerrar el servicio')
-connection.send(name.encode())
+
 
 # saludo1 = print('wena lo chicabros')
+try:
+	while True:
+		connection, addr = socket_servidor.accept()
+		print("Received connection from ", addr[0], "(", addr[1], ")\n")
+		print('Conexion establecida. conectado desde: {}, ({})'.format(addr[0], addr[0]))
+		#get a connection from client side
+		client_name = connection.recv(2048)
+		client_name = client_name.decode()
+		print(client_name + ' El cliente se a conectado.')
+		connection.send(name.encode())
+		while True:
+			try:
+				mensaje = 'volver'
+    			# message = input(dado(dado1,intentos))
+				if mensaje == 'salir()':
+					mensaje = 'Nos vemos Gracias por jugar!...'
+					connection.send(mensaje.encode())
+					print("\n")
+					break
 
-while True:
-    mensaje = 'Bienvenido'
-    # message = input(dado(dado1,intentos))
-    if mensaje == 'salir()':
-        mensaje = 'Good Night...'
-        connection.send(mensaje.encode())
-        print("\n")
-        break
-    if mensaje == 'Bienvenido':
-        mensaje = '--Bienvenido a la sala de juegos--\n1.- Juegos de dados\n2.- ...'
-        connection.send(mensaje.encode())
-        mensaje = connection.recv(1024)
-        mensaje = mensaje.decode()
-        print("Cliente -",client_name,">>",mensaje)
+				if mensaje == 'volver':
+					mensaje = '---Bienvenido a la sala de juegos---\n1.- Juegos de dados\n2.- Cachipun \n3.- Salir'
+					connection.send(mensaje.encode())
+					mensaje = connection.recv(2048)
+					mensaje = mensaje.decode()
+					print("Cliente -",client_name,">>",mensaje)
 
-        if mensaje == '1':
-            dado1 = []
-            j2 = dado_calcular(dado1,intentos)
-            mensaje = '--Juegos de dados--\n>> Lanzaras 3 veces un dado de 6 caras, aquel que saque el mayor gana\nPRECIONE UNA TECLA PARA CONTINUAR'
-            connection.send(mensaje.encode())
-            mensaje = connection.recv(2048)
-            mensaje = mensaje.decode()
-            print("Cliente -",client_name,">>",mensaje)
+					if mensaje == '1':
+						dado1 = []
+						j2 = dado_calcular(dado1,intentos)
+						mensaje = '--Juegos de dados--\n>> Lanzaras 3 veces un dado de 6 caras, aquel que saque el mayor gana\nPRECIONE UNA TECLA PARA CONTINUAR'
+						connection.send(mensaje.encode())
+						mensaje = connection.recv(2048)
+						mensaje = mensaje.decode()
+						print("Cliente -",client_name,">>",mensaje)
 
+						mensaje = connection.recv(2048)
+						mensaje = mensaje.decode()
+						# mensaje = int(mensaje)#"{}".format(mensaje)
+						print("Cliente -",client_name,">>",mensaje)
+						print("Servidor -",j2)
+						if int(mensaje) > j2 :
+							mensaje = "Gano el Cliente con {}\n Escriba <volver>, para volver al menu :D".format(mensaje)
+							connection.send(mensaje.encode())
+							mensaje = connection.recv(2048)
+							mensaje = mensaje.decode()
+							print("Cliente -",client_name,">>",mensaje)
+						else:
+							mensaje = "Gano el Servidor con {}\n Escriba <volver>, para volver al menu :D".format(j2)
+							connection.send(mensaje.encode())
+							mensaje = connection.recv(2048)
+							mensaje = mensaje.decode()
+							print("Cliente -",client_name,">>",mensaje)
 
-			# print("Cliente -",client_name,">>",mensaje)
+					if mensaje == '2':
+						mensaje = "---Cachipum---\n>> Bienvenido, vamos a jugar a Cachipum.\n>> Juguemos!!!.\n>> Elije: piedra, papel o tijera?"
+						resultado = cachipum()
+						connection.send(mensaje.encode())
+						mensaje = connection.recv(2048)
+						mensaje = mensaje.decode()
+						print("Cliente -",client_name,">>",mensaje)
+						print("Servidor -",resultado)
 
-            mensaje = connection.recv(2048)
-            mensaje = mensaje.decode()
-            mensaje = int(mensaje)#"{}".format(mensaje)
-            print("Cliente -",client_name,">>",mensaje)
-            print("Servidor -",j2)
-            if mensaje > j2 :
-                mensaje = "Gano el Cliente con {}".format(mensaje)
-                connection.send(mensaje.encode())
-                mensaje = connection.recv(2048)
-                mensaje = mensaje.decode()
-                print("Cliente -",client_name,">>",mensaje)
-            else:
-                mensaje = "Gano el Servidor con {}\n Escriba Bienvenido".format(j2)
-                connection.send(mensaje.encode())
-                mensaje = connection.recv(2048)
-                mensaje = mensaje.decode()
-                print("Cliente -",client_name,">>",mensaje)
+						if mensaje == resultado:
+							mensaje = "El pc tambien elijio {}, es un empate\n Escriba volver".format(resultado)
+							connection.send(mensaje.encode())
+							mensaje = connection.recv(2048)
+							mensaje = mensaje.decode()
+							print("Cliente -",client_name,">>",mensaje)
+						elif resultado == "tijera" and mensaje == "papel":
+							mensaje = "Gano el Servidor jaja - {}\n Escriba <volver>, para volver al menu :D".format(resultado)
+							connection.send(mensaje.encode())
+							mensaje = connection.recv(2048)
+							mensaje = mensaje.decode()
+							print("Cliente -",client_name,">>",mensaje)
+						elif resultado == "tijera" and mensaje == "piedra":
+							mensaje = "Gano el Cliente - {}\n Escriba <volver>, para volver al menu :D".format(mensaje)
+							connection.send(mensaje.encode())
+							mensaje = connection.recv(2048)
+							mensaje = mensaje.decode()
+							print("Cliente -",client_name,">>",mensaje)
+						elif resultado == "piedra" and mensaje == "tijera":
+							mensaje = "Gano el Servidor jaja - {}\n Escriba <volver>, para volver al menu :D".format(resultado)
+							connection.send(mensaje.encode())
+							mensaje = connection.recv(2048)
+							mensaje = mensaje.decode()
+							print("Cliente -",client_name,">>",mensaje)
+						elif resultado == "piedra" and mensaje == "papel":
+							mensaje = "Gano el Cliente - {}\n Escriba <volver>, para volver al menu :D".format(resultado)
+							connection.send(mensaje.encode())
+							mensaje = connection.recv(2048)
+							mensaje = mensaje.decode()
+							print("Cliente -",client_name,">>",mensaje)
+						elif resultado == "papel" and mensaje == "tijera":
+							mensaje = "Gano el Cliente - {}\n Escriba <volver>, para volver al menu :D".format(mensaje)
+							connection.send(mensaje.encode())
+							mensaje = connection.recv(2048)
+							mensaje = mensaje.decode()
+							print("Cliente -",client_name,">>",mensaje)
+						elif resultado == "papel" and mensaje == "piedra":
+							mensaje = "Gano el Servidor jaja - {}\n Escriba <volver>, para volver al menu :D".format(resultado)
+							connection.send(mensaje.encode())
+							mensaje = connection.recv(2048)
+							mensaje = mensaje.decode()
+							print("Cliente -",client_name,">>",mensaje)
+						else:
+							print ("Opcion incorrecta, vuleva a intentarlo")
+
+			except socket.error:
+				print ("Conexion terminada abruptamente por el cliente.")
+				print ("Cerrando conexion con el cliente ...")
+				connection.close()
+				print ("Conexion con el cliente cerrado.")
+				break
+
+			except KeyboardInterrupt:
+			    print ("\n∫Se interrunpio el cliente con un Control_C.")
+			    print ("Cerrando conexion con el cliente ...")
+			    connection.close()
+			    print ("Conexion con el cliente cerrado.")
+			    break
+
+except KeyboardInterrupt:
+    print ("\nSe interrumpio el servidor con un Control_C.")
+    #socket_cliente.close()
+    print ("Cerrando el servicio ...")
+    socket_servidor.close()
+    print ("Servicio cerrado, Adios!")
